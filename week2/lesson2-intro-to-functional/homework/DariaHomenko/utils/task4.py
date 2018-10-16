79a121e7a646ad7de8c3ef7b290ab381a115caff
Daria-Homenko <43683023+Daria-Homenko@users.noreply.github.com>
2018-10-16 22:32:31 +0300

    week2_lesson2_Daria_Homenko best solution

--- week2/lesson2-intro-to-functional/homework/DariaHomenko/utils/task4.py
+++ week2/lesson2-intro-to-functional/homework/DariaHomenko/utils/task4.py
@@ -1,3 +1,2 @@
 def members_with_o(members_list: list):
-    return list(filter(lambda member_item: member_item if 'o' in member_item['name'] else '', members_list))
-
+    return list(filter(lambda member_item: 'o' in member_item['name'].lower(), members_list))
