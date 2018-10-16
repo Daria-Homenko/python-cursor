1b2b76cd669619eb89955ce807a4ea43fc2ae838
Daria-Homenko <43683023+Daria-Homenko@users.noreply.github.com>
2018-10-16 22:37:24 +0300

    week2_lesson2_Daria_Homenko Added best solution

--- week2/lesson2-intro-to-functional/homework/DariaHomenko/utils/task6.py
+++ week2/lesson2-intro-to-functional/homework/DariaHomenko/utils/task6.py
@@ -1,3 +1,2 @@
 def sort_members(member_list: list):
-    return sorted(sorted(member_list, key=(lambda member_item: member_item['age'])),
-                  key=(lambda member_item: len(member_item['name'])))
+    return sorted(member_list, key=lambda member_item: (len(member_item['name']), member_item['age']))
